73b05d34fe4f5ceeeb038b5b308a908e218522b8
klwebbers <webmessager8904>
2022-02-17 16:09:59 +0530

    mobile toggle fixed

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,20 @@ $('#siteNav').affix({
 		top: 100
 	}
 })
+$("#submit-form").submit((e)=>{
+	e.preventDefault()
+	$.ajax({
+		url:"https://script.google.com/macros/s/AKfycbwfiXlJFdhqweC2pEEwb7VoILQGnx4eRGExGUER2u540mNo_qhM79xcUEt_DHAsP8vQ/exec",
+		data:$("#submit-form").serialize(),
+		method:"post",
+		success:function (response){
+			alert("Form submitted successfully")
+			window.location.reload()
+			//window.location.href="https://google.com"
+		},
+		error:function (err){
+			alert("Something Error")
+
+		}
+	})
+})
